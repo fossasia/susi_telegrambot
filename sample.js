5f52874cbacf98e5be4009cd49f6a8ab0bab0afc
sudheesh001 <sudheesh1995@outlook.com>
2016-07-22 13:53:36 +0530

    Adding codacy to the repository and cleaning up code Travis testing and codecov activation Code coverage and fixes

--- sample.js
+++ sample.js
@@ -1,13 +1,12 @@
-var request = require("request")
+var request = require('request');
 
-var url = "http://localhost:9000/api/susi.json?q=Hi I am Sudheesh"
+var queryUrl = 'http://localhost:9000/api/susi.json?q=Hi I am Sudheesh';
 
 request({
-    url: url,
+    url: queryUrl,
     json: true
 }, function (error, response, body) {
-
     if (!error && response.statusCode === 200) {
-        console.log(body.answers[0].actions[0].expression); // Print the json response
+        return body.answers[0].actions[0].expression;
     }
-})
+});
